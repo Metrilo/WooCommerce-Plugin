000ac2b39233083b0c3736a2b199b06049da75e9
Ned <ned@metrilo.com>
2021-01-11 14:55:41 +0200

    KYP-1321: Updated cookie reset script to use vanilla js instead of jQuery

--- views/render-footer-tracking-events.php
+++ views/render-footer-tracking-events.php
@@ -1,15 +1,8 @@
 <?php if ($this->has_events_in_cookie): ?>
     <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            $.get("<?php echo add_query_arg('metrilo_clear', 1); ?>", function(response) {  });
-        });
-        
         document.addEventListener("DOMContentLoaded",function() {
-            console.log(123);
-            var httpRequest = new XMLHttpRequest();
-            httpRequest.onreadystatechange = function (data) {
-                // code
-            }
+            const url         = "<?php echo add_query_arg('metrilo_clear', 1); ?>";
+            const httpRequest = new XMLHttpRequest();
             httpRequest.open('GET', url);
             httpRequest.send();
         });
