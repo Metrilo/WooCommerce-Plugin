898cfb0266e773a76514e7293a7af0d4a6ec2a64
nedick007 <40760980+nedick007@users.noreply.github.com>
2021-07-05 15:01:46 +0300

    KYP-1917: Fixed module configuration panel (#63)

--- helpers/class.metrilo-activity.php
+++ helpers/class.metrilo-activity.php
@@ -5,19 +5,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Metrilo_Activity {
     
-    public function create_activity($type, $activity_endpoint_domain)
+    public function create_activity($type, $activity_endpoint_domain, $token = '', $secret = '')
     {
-        $module_options = get_option('woocommerce_metrilo-analytics_settings', false);
-        $token          = $module_options['api_token'];
-        $secret         = $module_options['api_secret'];
+        if(empty($token) || empty($secret)) {
+            $module_options = get_option('woocommerce_metrilo-analytics_settings', false);
+            $token          = $module_options['api_token'];
+            $secret         = $module_options['api_secret'];
+        }
+        
         $end_point      = 'https://' . $activity_endpoint_domain;
         $api_client     = new Metrilo_Api_Client();
-        $client         = $api_client->get_client($end_point);
+        $client         = $api_client->get_client($end_point, $token, $secret);
         
-        $data = array(
-            'type'   => $type,
-            'secret' => $secret
-        );
+        $data = [
+            'type' => $type
+        ];
         
         $url = $end_point . '/tracking/' . $token . '/activity';
         
--- helpers/class.metrilo-api-client.php
+++ helpers/class.metrilo-api-client.php
@@ -2,15 +2,19 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+
 require_once(METRILO_ANALYTICS_PLUGIN_PATH . 'api/class.metrilo-client.php');
 
 class Metrilo_Api_Client {
     
-    public function get_client($api_endpoint_domain)
+    public function get_client($api_endpoint_domain, $token = '', $secret = '')
     {
-        $module_options = get_option('woocommerce_metrilo-analytics_settings', false);
-        $token          = $module_options['api_token'];
-        $secret         = $module_options['api_secret'];
+        if(empty($token) || empty($secret)) {
+            $module_options = get_option('woocommerce_metrilo-analytics_settings', false);
+            $token          = $module_options['api_token'];
+            $secret         = $module_options['api_secret'];
+        }
+        
         $platform       = 'Wordpress ' . get_bloginfo( 'version' );
         $plugin_version = METRILO_ANALYTICS_PLUGIN_VERSION;
         $url            = 'https://' . $api_endpoint_domain;
